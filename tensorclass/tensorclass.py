ec57da0de92be8015abe737d1a3cc9bc9b97b9e7
Oliver Batchelor <oliver.batchelor@canterbury.ac.nz>
2023-05-16 23:29:45 +1200

    .

--- tensorclass/tensorclass.py
+++ tensorclass/tensorclass.py
@@ -1,4 +1,5 @@
 from dataclasses import InitVar, asdict, dataclass, field, fields, replace
+from numbers import Number
 import torch
 
 from torch import Tensor
@@ -25,32 +26,100 @@ def add_batch_dim(t, name='N', broadcast=True):
 @dataclass(kw_only=True, repr=False)
 class TensorClass():
   # Broadcast prefixes shapes together
-  broadcast:  InitVar[bool] = False   
+  broadcast:  InitVar[bool] = False
+  batched:  InitVar[bool] = True
 
 
-  def check_shapes(self, broadcast=False, memo=None, variadic_memo=None, variadic_broadcast_memo=None):
+  def check_shapes(self, broadcast=False, batched=True, memo=None, variadic_memo=None, variadic_broadcast_memo=None):
     memo = {} if memo is None else memo
     variadic_memo = {} if variadic_memo is None else variadic_memo
     variadic_broadcast_memo = {} if variadic_broadcast_memo is None else variadic_broadcast_memo
 
+    self.batch_size = ()
+    self.fields = {}
+
+
     for f in fields(self):
-      value = getattr(self, f.name)
-      batch_size = variadic_memo.get('N', None) if broadcast is False else variadic_broadcast_memo.get('N', None)
-      
+      value = getattr(self, f.name)    
       if isinstance(value, torch.Tensor):
-        t = add_batch_dim(f.type, broadcast=broadcast)
+        t = add_batch_dim(f.type, broadcast=broadcast) if batched else f.type
 
         if not t._check_shape(value, single_memo=memo, variadic_memo=variadic_memo, variadic_broadcast_memo=variadic_broadcast_memo):
-          if batch_size is not None:
-            batch = f"batch {tuple(*batch_size)}, "
-          else:
-            batch = ""
+
+          if self.batch_size is not None:
+            batch = f"batch {tuple(*self.batch_size)}, "
 
           class_name = self.__class__.__name__
           raise TypeError(f"{class_name}.{f.name}: bad shape {value.shape}, for {batch}shape ({t.dim_str}), broadcast={broadcast}")
         
       elif isinstance(value, TensorClass):
         value.check_shapes(broadcast=broadcast, memo=memo, variadic_memo=variadic_memo, variadic_broadcast_memo=variadic_broadcast_memo)
+
+      self.batch_size = variadic_memo.get('N', None) if broadcast is False else variadic_broadcast_memo.get('N', None)
+
+
+  def __iter__(self):
+    fs = fields(self)
+    for f in fs:
+      yield (f.name, getattr(self, f.name))
+
+  def map(self, func):
+    def f(t):
+      if isinstance(t, torch.Tensor):
+        return func(t)
+      elif isinstance(t, TensorClass):
+        return t.map(func)
+      else:
+        return t
+
+    d = {k:f(t) for k, t in iter(self)}
+    return self.__class__(**d)
+
+  def __getitem__(self, slice):
+    return self.map(lambda t: t[slice])
+
+  def to(self, device):
+    return self.map(lambda t: t.to(device))
+
+  def expand(self, shape):
+    return self.map(lambda t: t.expand(shape))
+
+  @classmethod
+  def empty(cls:type, shape=(), device='cpu', **kwargs):
+    if isinstance(shape, Number):
+      shape = (shape,)
+    
+    def make_tensor(k, info):
+      if k in kwargs:
+        return kwargs[k]
+
+      if info is None:
+        raise RuntimeError(f"{k}: has no argument or tensor annotation")
+
+      if info.dtype is None:
+        raise RuntimeError(f"{k}: has no dtype annotation")
+      
+      field_shape = (d.size for d in info.shape.dims)
+      return torch.empty( tuple( (*shape, *field_shape) ), 
+        dtype=info.dtype.dtype, device=device)
+
+
+    return cls(**{f.name:make_tensor(f.name, self.(f.type)) 
+      for f in fields(cls)}) 
+
+
+
+
+  def unsqueeze(self, dim):
+    assert dim <= len(self.shape), f"Cannot unsqueeze dim {dim} in shape {self.shape}"
+    return self.map(lambda t: t.unsqueeze(dim))
+
+
+  def __repr__(self):
+    name= self.__class__.__name__
+    return f"{name}({shape(asdict(self))})"
+
+
 
   def __post_init__(self, broadcast):
     self.check_shapes(broadcast=broadcast)
